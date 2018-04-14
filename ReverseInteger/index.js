6067201ab57fca6fef0caa6c954028abe8877cd1
tusharc88 <tushar54983@gmail.com>
2018-04-13 20:41:59 -0700

    Adding another solution to ReverseInteger that correctly checks 32 bit integer overflow

--- ReverseInteger/index.js
+++ ReverseInteger/index.js
@@ -2,26 +2,42 @@
  * @param {number} x
  * @return {number}
  */
+
+// I was not able to correctly check for 32 bit integer overflow here as Javascript accepts integers over 32 bits
+// var reverse = function(x) {
+//   const flag = x < 0 ? -1 : 1;
+//   x = x * flag;
+//
+//   let reverseStr = x
+//     .toString()
+//     .split('')
+//     .reverse()
+//     .join('');
+//   const reverseReverseStr = reverseStr
+//     .split('')
+//     .reverse()
+//     .join('');
+//
+//   // check for overflow
+//   if (reverseReverseStr !== x.toString()) {
+//     reverseStr = '0';
+//   }
+//
+//   return parseInt(reverseStr) * flag;
+// };
+
 var reverse = function(x) {
-  const flag = x < 0 ? -1 : 1;
-  x = x * flag;
+  const reversed =
+    parseInt(
+      Math.abs(x)
+        .toString()
+        .split('')
+        .reverse()
+        .join('')
+    ) * Math.sign(x);
 
-  let reverseStr = x
-    .toString()
-    .split('')
-    .reverse()
-    .join('');
-  const reverseReverseStr = reverseStr
-    .split('')
-    .reverse()
-    .join('');
-
-  // check for overflow
-  if (reverseReverseStr !== x.toString()) {
-    reverseStr = '0';
-  }
-
-  return parseInt(reverseStr) * flag;
+  // condition to check overflow
+  return reversed <= 0x7fffffff && reversed >= -0x80000000 ? reversed : 0;
 };
 
 module.exports = reverse;
